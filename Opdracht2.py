9a2bbfac8fa41d91d6c4ffdaf6407cfb54092e0c
Del161 <d.vegter@st.hanze.nl>
2022-10-23 16:04:45 +0000

    added error message and functionality

--- Opdracht2.py
+++ Opdracht2.py
@@ -25,6 +25,29 @@ amino_acids_weight_dict = {
 #unkown aminoacid gets the avarage weight of an aminoacid which is 110
 }
 
+hydrophilicity_dict = {
+"I" :	4.5,
+"V" :	4.2,
+"L" :	3.8,
+"F" :	2.8,
+"C" :	2.5,
+"M" :	1.9,
+"A" :	1.8,
+"G" :	-0.4,
+"T" :	-0.7,
+"S" :	-0.8,
+"W" : 	-0.9,
+"Y" :	-1.3,
+"P" :	-1.6,
+"H" :	-3.2,
+"E" :	-3.5,
+"Q" :	-3.5,
+"D" :	-3.5,
+"N" :	-3.5,
+"K" :	-3.9,
+"R" :	-4.5
+}
+
 def open_file(input_name, comparison_name):
         # read pdb file
     with open(input_name, "r") as file:
@@ -74,6 +97,7 @@ def get_helix_sheet_strings(amino_acid_string, list_lines):
             sheet_residue_end = lines[34:37].split()
             sheet_amino_acids = amino_acid_string[int(sheet_residue_start[0]): int(sheet_residue_end[0])]
             sheet_sequence += sheet_amino_acids
+    return helix_sequence, sheet_sequence
 
 #retrieves all aminoacids from the file
 def amino_acids(list_lines):
@@ -107,13 +131,42 @@ def calculate_aminoacid_weight(amino_acid_string):
     return aminoacid_weight
 
 def compare_files(comparison_lines, aminoacids_end):
+    #predefined variaaableees
+    comparison_list = []
+    aminoacids_list = []
+    endresult_compared_list = ""
+    rownumber1 = 0
+    rownumber = 0
+
+    #remove newlines from fasta sequence, check if its not header
     for lines in comparison_lines:
-        lines = lines.replace("\n", "")
-        print(lines)
+        if not lines.startswith(">"):
+            lines = lines.replace("\n", "")
+            comparison_list.append("fasta sequence line: "+ str(rownumber) + " :"+ lines)
+            rownumber += 1
+
+    #remove newlines from pdb sequence
+    aminoacids_string = aminoacids_end.replace("\n", "")
+
+    for rows in range(0, len(aminoacids_string), 70):
+        aminoacids_list.append("PDB sequence   line: " + str(rownumber1)+ " :"+ aminoacids_string[rows: rows + 70])
+        rownumber1 += 1
+
+    #check which list is shortest to prevent index errors
+    if len(aminoacids_list) > len(comparison_list):
+        length_list = len(comparison_list)
+    else: length_list = len(aminoacids_list)
+
+    #save the formatted lines to a list
+    for linecount in range(length_list):
+        endresult_compared_list += (comparison_list[linecount]+ "\n"+ aminoacids_list[linecount]+ "\n \n")
+
+    return endresult_compared_list
+    
 
 
-def write_results(output_name, total_weight, aminoacids_end, aminoacid_weight):
-    #write the results to a file
+def write_results(output_name, total_weight, aminoacid_weight, aminoacids_end, sheet_sequence, helix_sequence, endresult_compared_list ):
+    #write the weight results to a file
     #create given output filename
     with open(output_name, "w") as writefile:
         # loop items and write it to the file
@@ -123,8 +176,18 @@ def write_results(output_name, total_weight, aminoacids_end, aminoacid_weight):
         writefile.write("The weight of the aminoacids is around: ")
         writefile.write(str(aminoacid_weight))
         writefile.write("\n \n")
-        writefile.write("The aminoacid sequence \n")
+
+        #write the various sequences to file
+        writefile.write("The aminoacid sequence: \n")
         writefile.write(aminoacids_end)
+        writefile.write("\n \n The helix sequence: \n")
+        writefile.write(helix_sequence)
+        writefile.write("\n \n The sheet sequence: \n")
+        writefile.write(sheet_sequence)
+
+        #write the compared files to file to compare them easily
+        writefile.write("\n \n the files compared below eachother: \n")
+        writefile.write(endresult_compared_list)
 
 def main():
     # get the given arguments
@@ -135,7 +198,7 @@ def main():
     comparison_name = ""
 
     if len(arguments) < 2:
-        raise Exception("No input filename given")
+        raise Exception("No input filename given, please use commandline arguments. (python3 script2_2.py PDB file inputname, Fasta inputname, output filename )")
     else:
         # get the argument and set the variable
         input_name = arguments[1]
@@ -158,10 +221,10 @@ def main():
     total_weight = calculate_weight(list_lines)
     aminoacids_end, amino_acid_string  = amino_acids(list_lines)
     aminoacid_weight = calculate_aminoacid_weight(amino_acid_string)
-    get_helix_sheet_strings(amino_acid_string, list_lines)
-    compare_files(comparison_lines, aminoacids_end)
-    write_results(output_name, total_weight, aminoacids_end, aminoacid_weight)
-    print("information logged in ", output_name)
+    helix_sequence, sheet_sequence = get_helix_sheet_strings(amino_acid_string, list_lines)
+    endresult_compared_list = compare_files(comparison_lines, aminoacids_end)
+    write_results(output_name, total_weight, aminoacid_weight, aminoacids_end, sheet_sequence, helix_sequence, endresult_compared_list)
+    print("information and results were logged in ", output_name)
 
 if __name__ == "__main__":
     main()
