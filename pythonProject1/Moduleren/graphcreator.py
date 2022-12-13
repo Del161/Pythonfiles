ce12af991a537f7048a7d300e69af628fd22c46f
dvegter <d.vegter@st.hanze.nl>
2022-12-13 15:59:24 +0100

    updated and improved added boxplots

--- pythonProject1/Moduleren/graphcreator.py
+++ pythonProject1/Moduleren/graphcreator.py
@@ -71,33 +71,60 @@ def boxplot(values_headers):
     """
     Creates a boxplot from the given values
     """
-    categories = {}
+
+    # predefined variables
+    headers_numerated = []
     values = values_headers[0]
     headers1 = values_headers[1]
     tick_values = []
     data = []
+    plot_data = []
+    counter = 0
+    user_choice = ""
+    x_labels = []
 
+    for headers in headers1[1:]:
+        headers_numerated.append(str(counter) + " : " + headers + "\n")
+        counter += 1
+
+    if len(headers1) > 2:
+        print("There are multiple values, please select the ones you want to use for the bar chart\n"
+              "the input method is 1,2,3,4 etc or 1,4,6 etc, use * for all categories \n",
+              *headers_numerated)
+        user_choice = input("please enter the values of the wanted categories: ")
+
+    # make the lists for use in the boxplot
     for count in range(len(headers1[1:])):
         for individual_labels in values:
             individual_values = values[individual_labels]
             tick_values.append(int(individual_values[int(count)]))
         data.append(tick_values)
-
         tick_values = []
 
+    # act upon user choice
+    if user_choice == "*":
+        plot_data = data
+        x_labels = headers1[1:]
+    else:
+        user_choice = user_choice.split(",")
+        for numbers in user_choice:
+            plot_data.append(data[int(numbers)])
+            x_labels.append(headers1[int(numbers)+1])
 
+    # plot the boxplot with all the data
     fig = plt.figure(figsize=(10, 7))
     ax = fig.add_axes([0.15, 0.15, 0.85, 0.85])
-    ax.set_xticklabels(headers1[1:])
-    fig.autofmt_xdate()
-    bp = ax.boxplot(data)
+    ax.set_xticklabels(x_labels)
+    plt.setp(ax.get_xticklabels(), rotation=10, horizontalalignment='right')
+
+    bp = ax.boxplot(plot_data)
     plt.show()
 
 def main():
     """
     one who rules
     """
-    print("bargraphcreator.py main")
+    print("graphcreator.py main")
 
 
 # to protect against problems when imported
